a4107c92c90398232c0bd3b395306bc00075b6e0
mko101 <m.koster3@student.vu.nl>
2025-02-20 12:22:46 +0100

    Corrected two mistakes + added comments

--- scripts/part3.py
+++ scripts/part3.py
@@ -10,35 +10,55 @@ cur = con.cursor()
 
 # Step 3: compute the sleep duration for each moment of sleep of an individual
 def compute_sleep_duration(user_id):
+    # gets data from the database and adds it to a dataframe
     sleep_duration = cur.execute(f"SELECT Id, date, logId FROM minute_sleep WHERE Id={user_id}")
     rows = sleep_duration.fetchall()
     df = pd.DataFrame(rows, columns = [x[0] for x in cur.description])
+
+    # converts the date column to the type datetime
     df["date"] = pd.to_datetime(df["date"])
 
+    # calculates the time asleep for each logId 
     sleep_durations = df.groupby('logId').agg(start_time=('date', 'min'), end_time=('date', 'max')).reset_index()
     sleep_durations['MinutesSlept'] = (sleep_durations['end_time'] - sleep_durations['start_time']).dt.total_seconds() / 60
-    sleep_durations['Date'] = sleep_durations['start_time'].dt.date
-    sleep_durations = sleep_durations.groupby('Date')['MinutesSlept'].sum().reset_index()
+    
+    # takes the date to be the day the user woke up
+    sleep_durations['Date'] = sleep_durations['end_time'].dt.date
+
+    # drops the unnecessary columns from the dataframe
+    sleep_durations = sleep_durations.drop(["logId", "start_time", "end_time"], axis=1)
     
     return sleep_durations
 
+print(compute_sleep_duration(1503960366))
+
 # Step 4: analyse the relationship between the duration of sleep and the active minutes for an individual
 def compare_activity_and_sleep(user_id):
+    # gets data from the database and adds it to a dataframe
     active_minutes = cur.execute(f"SELECT Id, ActivityDate, VeryActiveMinutes, FairlyActiveMinutes, LightlyActiveMinutes FROM daily_activity WHERE Id={user_id}")
     rows = active_minutes.fetchall()
     df = pd.DataFrame(rows, columns = [x[0] for x in cur.description])
+
+    # converts the date column to the type datetime
     df["ActivityDate"] = pd.to_datetime(df["ActivityDate"]).dt.date
 
+    # calculates the time the user is active by summing the active minutes for that day
     df['ActiveMinutes'] = (df['VeryActiveMinutes'] + df['FairlyActiveMinutes'] + df['LightlyActiveMinutes'])
     daily_activity = df.groupby('ActivityDate')['ActiveMinutes'].sum().reset_index()
 
+    # renames the ActivityDate column to Date
     daily_activity.rename(columns={"ActivityDate": "Date"}, inplace=True)
 
+    # sums the duration of sleep based on date
     daily_sleep = compute_sleep_duration(1503960366)
+    daily_sleep = daily_sleep.groupby('Date')['MinutesSlept'].sum().reset_index()
+
+    # merges the total active minutes and the duration of sleep based on the date, leaving out any dates where no active minutes or sleep duration is available
     data_sleep_and_activity = pd.merge(daily_activity, daily_sleep, on="Date", how="inner")
 
-    X = data_sleep_and_activity['ActiveMinutes'] # decision variable
-    y = data_sleep_and_activity['MinutesSlept'] # dependent variable
+    # linear regression model
+    X = data_sleep_and_activity["MinutesSlept"] # independent variable
+    y = data_sleep_and_activity["ActiveMinutes"] # dependent variable
 
     X = sm.add_constant(X)
 
@@ -47,8 +67,8 @@ def compare_activity_and_sleep(user_id):
     print(model.summary())
 
     plt.figure(figsize=(10, 6))
-    plt.scatter(data_sleep_and_activity["ActiveMinutes"], data_sleep_and_activity["MinutesSlept"], color="skyblue", label="Data points")
-    plt.plot(data_sleep_and_activity["ActiveMinutes"], model.predict(X), color="red", label="Regression line")
+    plt.scatter(data_sleep_and_activity["MinutesSlept"], data_sleep_and_activity["ActiveMinutes"], color="skyblue", label="Data points")
+    plt.plot(data_sleep_and_activity["MinutesSlept"], model.predict(X), color="red", label="Regression line")
     plt.xlabel("Active Minutes")
     plt.ylabel("Minutes Slept")
     plt.title(f"Linear Relationship between Active Minutes and Minutes Slept for User {user_id}")
