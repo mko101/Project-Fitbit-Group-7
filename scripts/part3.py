21775d5327fc437fba249eea35ca80b7ddcf8ed4
mko101 <m.koster3@student.vu.nl>
2025-02-26 19:43:17 +0100

    Made the calculation of the fat percentage more realistic by not sampling again if gender and age are already randomly sampled

--- scripts/part3.py
+++ scripts/part3.py
@@ -552,17 +552,30 @@ def visualize_weather_activity():
 
 
 # Step 9: look for missing values in the weight_log and resolve them
-def sample_random_age(random_number):
+gender_users = {}
+age_users = {}
+
+def sample_random_age(user):
     # sample = {"18-24": 0.1579, "25-34": 0.2593, "35-44": 0.1940, "45-54": 0.1608, "55-64": 0.1294, "65+": 0.0987}
     age_groupes = {0.1579: list(range(18, 25)), 0.2593: list(range(25, 35)), 0.1940: list(range(35, 45)), 0.1608: list(range(45, 55)), 0.1294: list(range(55, 65)), 0.0987: list(range(65, 79))}
 
-    sum = 0
+    if user not in age_users:
+        sum = 0
+        random_number = random.random()
 
-    for probabilty, ages in age_groupes.items():
-        sum += probabilty
+        for probabilty, ages in age_groupes.items():
+            sum += probabilty
 
-        if random_number <= sum:
-            return random.choice(ages)
+            if random_number <= sum:
+                age_users[user] = random.choice(ages)
+
+    return age_users[user]
+
+def sample_random_gender(user):
+    if user not in gender_users:
+        gender_users[user] = "F" if bernoulli.rvs(0.4466) else "M"
+    
+    return gender_users[user]
 
 
 def resolve_missing_values_weight_log():
@@ -581,7 +594,7 @@ def resolve_missing_values_weight_log():
         # https://www.coolest-gadgets.com/fitbit-statistics/
         # F: 44.66% and M: 55.34%
         # 18-24: 0.1579, 25-34: 0.2593, 35-44: 0.1940, 45-54: 0.1608, 55-64: 0.1294, 65+: 0.0987
-        df["Fat"] = df.apply(lambda row: ((row["BMI"] * 1.2) + (0.23 * sample_random_age(random.random())) - (5.4 if bernoulli.rvs(0.4466) else 16.2)) if pd.isnull(row["Fat"]) else row["Fat"], axis=1)
+        df["Fat"] = df.apply(lambda row: ((row["BMI"] * 1.2) + (0.23 * sample_random_age(row["Id"])) - (5.4 if sample_random_gender(row["Id"]) == "F" else 16.2)) if pd.isnull(row["Fat"]) else row["Fat"], axis=1)
         
     print(df.head(33))
 
