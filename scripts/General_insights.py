35ece1aaa3fa40ad230f20619d0e1dcae4c209a7
mko101 <m.koster3@student.vu.nl>
2025-03-12 14:14:04 +0100

    Added a check to see if the end date is after the start date

--- scripts/General_insights.py
+++ scripts/General_insights.py
@@ -1,7 +1,10 @@
 # IMPORTS 
 import streamlit as st
+import pandas as pd
 import datetime
 import part1
+
+from datetime import timedelta
 
 st.set_page_config(
     page_title="Fitbit Dashboard",
@@ -22,7 +25,13 @@ with st.sidebar:
         st.session_state.user = user
         st.switch_page("pages/1_User-specific_data.py")
 
-    start_date = st.date_input("Select a start date:", datetime.date(2016, 3, 12), min_value=datetime.date(year=2016, month=3, day=12), max_value=datetime.date(year=2016, month=4, day=12))
-    end_date = st.date_input("Select a end date:", datetime.date(2016, 4, 12), min_value=datetime.date(year=2016, month=3, day=12), max_value=datetime.date(year=2016, month=4, day=12))
+    start_date = st.date_input("Select a start date:", datetime.date(2016, 3, 12), min_value=datetime.date(year=2016, month=3, day=12), max_value=datetime.date(year=2016, month=4, day=12), format="MM/DD/YYYY")
+    end_date = st.date_input("Select an end date:", datetime.date(2016, 4, 12), min_value=datetime.date(year=2016, month=3, day=12), max_value=datetime.date(year=2016, month=4, day=12), format="MM/DD/YYYY")
+
+    if start_date and end_date:
+        if start_date > end_date:
+            st.error("Please ensure that the end date is later than the start date.")
+        else:
+            dates = pd.date_range(start_date, end_date, freq='d').strftime("%m/%d/%Y")
 
 st.write("This is the general page")
